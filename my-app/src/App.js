d62fd2c4a63226c66146397bed0359875f255449
JacobGuiro <jguiro09@gmail.com>
2022-11-16 19:04:10 -0600

    "Imported Material UI, styled page"

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,84 @@
-import logo from './logo.svg';
 import './App.css';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import Icon from "@mui/material/Icon";
+
+function createValAccountObject(accountName, username, password, image, gainLoss) {
+  return { accountName, username, password, image, gainLoss };
+};
+
+const rows = [
+  createValAccountObject('test#1223', 'Sample name', "sample password", "sample image", "sample gain"), // Returns an Object for the account
+  createValAccountObject('test#1423', 'Sample name', "sample password", "sample image", "sample gain"),
+  createValAccountObject('test#1523', 'Sample name', "sample password", "sample image", "sample gain"),
+  createValAccountObject('test#16223', 'Sample name', "sample password", "sample image", "sample gain"),
+  createValAccountObject('test#1123', 'Sample name', "sample password", "sample image", "sample gain"),
+];
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <div className='Container'>
+        <div className="Table-Container">
+        <TableContainer sx={{ width: '65%' }} component={Paper}>
+          <Table aria-label="valorant-account-tables">
+            <TableHead>
+              <TableRow>
+                <TableCell>Account Name</TableCell>
+                <TableCell>Username</TableCell>
+                <TableCell>Password</TableCell>
+                <TableCell>Rank</TableCell>
+                <TableCell>Last Match Rating</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow
+                  key={row.accountName}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {row.accountName}
+                  </TableCell>
+                  <TableCell>
+                    <div className="table-username">
+                      {row.username} 
+                      <VisibilityIcon /> 
+                    </div>
+                  </TableCell>
+                  <TableCell>
+                    <div className="table-username">
+                      {row.password} 
+                      <VisibilityIcon /> 
+                    </div>
+                  </TableCell>
+                  <TableCell>{row.image}</TableCell>
+                  <TableCell>{row.gainLoss}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        </div>
+      </div>
+    </ThemeProvider>
   );
 }
 

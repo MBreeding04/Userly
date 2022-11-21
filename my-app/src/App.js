f6c9acdc6d03205f38fe9765e342ac659f54bc20
Matthew <03mattman@gmail.com>
2022-11-21 07:58:40 -0500

    bug fixes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -24,7 +24,6 @@ const darkTheme = createTheme({
 });
 
 function App() {
-
   const [loading, setLoading] = useState(false);
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
@@ -34,7 +33,7 @@ function App() {
   var tempLogin = '';
   var tempPassword = '';
   var indexRef = -1;
-  var snd = new Audio("C:\Users\Mateo\Desktop\Userly\my-app\Bow.wav")
+  var snd = new Audio("./Bow.wav")
 
   useEffect(() => {
     if (firstRender.current) {
@@ -43,16 +42,14 @@ function App() {
     }
 
   })
+
   function DeleteRow(id) {
     var data = localStorage.getItem("accounts");
     data = JSON.parse(data);
     var temp = data.splice(id, 1);
     let final = [];
     for (let i = 0; i < data.length; i++) {
-      if (data[i].userName === temp.userName) {
-
-      }
-      else {
+      if (data[i].userName !== temp.userName) {
         const g = new Account(data[i].userName, data[i].userTag, data[i].loginName, data[i].password, data[i].image, data[i].gainLoss);
         final.push(g);
         localStorage.setItem("accounts", JSON.stringify(final));
@@ -60,6 +57,7 @@ function App() {
     }
     window.location.reload(false);
   }
+
   const start = () =>{
     snd.play()
   }
@@ -84,7 +82,6 @@ function App() {
           const password = accounts[i].password.toString();
           const image = api.data.images.small.toString();
           var gainLoss = api.data.ranking_in_tier.toString();
-
           let temp = new Account(userName, userTag, loginName, password, image, gainLoss);
           setRows(rows => [...rows, temp])
         }
@@ -101,70 +98,70 @@ function App() {
     }
   };
 
-
   if (loading) {
     return <div>Loading...</div>;
   }
   return (
     <ThemeProvider theme={darkTheme}>
-      <button onClick={start}>Play</button>
+      <audio controls>
+      <source src="/Bow.mp3" type="audio/mpeg"/>
+        </audio>
       <CssBaseline />
       <div className='Container'>
         <div className="Table-Container">
-          <TableContainer sx={{ width: '65%' }} component={Paper}>
+          <TableContainer sx={{ width: '65%' }} component={Paper} key={1000}>
             <div className='tableOfData'>
               <Table aria-label="valorant-account-tables">
-                <TableHead>
+                <TableHead key={1001}>
                   <TableRow key={indexRef}>
-                    <TableCell><Typography variant='h6'>User Name</Typography></TableCell>
-                    <TableCell><Typography variant='h6'>Login Name</Typography></TableCell>
-                    <TableCell><Typography variant='h6'>Password</Typography></TableCell>
-                    <TableCell><Typography variant='h6'>Rank</Typography></TableCell>
-                    <TableCell><Typography variant='h6'>RR In Rank</Typography></TableCell>
-                    <TableCell><Typography variant='h6'><div className="deleteLabel">Delete</div></Typography></TableCell>
+                    <TableCell key={1008}></TableCell>
+                    <TableCell key={1002}><Typography variant='h6'>User Name</Typography></TableCell>
+                    <TableCell key={1003}><Typography variant='h6'>Login Name</Typography></TableCell>
+                    <TableCell key={1004}><Typography variant='h6'>Password</Typography></TableCell>
+                    <TableCell key={1005}><Typography variant='h6'>Rank</Typography></TableCell>
+                    <TableCell key={1006}><Typography variant='h6'>RR In Rank</Typography></TableCell>
+                    <TableCell key={1007}><Typography variant='h6'><div className="deleteLabel">Delete</div></Typography></TableCell>
                   </TableRow>
                 </TableHead>
-                <TableBody>
+                <TableBody key={1008}>
                   {rows.map((row) => (
                     indexRef++,
                     row.id = indexRef,
                     tempLogin = row.loginName,
                     tempPassword = row.password,
                     <TableRow 
-                      key={row.accountName}
+                      key={"p"+row.id}
                       sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                     >
-                      <TableCell component="th" scope="row">
+                      <TableCell component="th" scope="row" key={row.id}>
                         {row.accountName}
                       </TableCell>
-                      <TableCell>
+                      <TableCell key={"q"+row.id}>
                         <Typography variant='h6'>{row.userName}</Typography>
                       </TableCell>
-                      <TableCell>
+                      <TableCell key={"w"+row.id}>
                         <div className="table-username">
                           <ShowAndHidePassword sendData={tempLogin}></ShowAndHidePassword>
                         </div>
                       </TableCell>
-                      <TableCell>
+                      <TableCell key={"e"+row.id}>
                         <div className="table-username">
                           <ShowAndHidePassword sendData={tempPassword}></ShowAndHidePassword>
                         </div>
                       </TableCell>
-                      <TableCell><img className="rank" src={row.image} alt="Rank" /></TableCell>
-                      <TableCell> <Typography sx={{
+                      <TableCell key={"r"+row.id}><img className="rank" src={row.image} alt="Rank" /></TableCell>
+                      <TableCell key={"t"+row.id}> <Typography sx={{
                         color:"text.primary"
                       }} variant='h6'><div className='gainLoss'>{row.gainLoss}</div></Typography></TableCell>
-                      <TableCell>
+                      <TableCell key={"y"+row.id}>
                         <div className='deleteButton'>
                           <Button color="error" onClick={() => DeleteRow(row.id)} variant="outlined" className='DeleteButton'>Delete</Button>
                         </div>
                       </TableCell>
                     </TableRow>
-
                   ))}
                   <TableRow key={4}></TableRow>
                 </TableBody>
-
               </Table>
             </div>
             <div className='AddUser'>
@@ -179,14 +176,15 @@ function App() {
                 aria-labelledby="modal-modal-title"
                 aria-describedby="modal-modal-description"
               >
+                <div>
                 <ModalBody />
+                </div>
               </Modal>
             </div>
           </TableContainer>
         </div >
       </div>
     </ThemeProvider>
-
   );
 }
 export default App;
